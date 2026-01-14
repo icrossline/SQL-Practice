1e9ab07ce2c633aceee57c20f68061abc8c38a2e
Abhishek Uniyal <abhiuni02@gmail.com>
2026-01-14 18:23:19 +0530

    Update questions.sql

--- Scripts/questions.sql
+++ Scripts/questions.sql
@@ -1,1 +1,29 @@
+-- 1.Display all employee details.
+
+SELECT *FROM Employees;
+
+
+--2. Show only employees working in the IT department.
+
+SELECT  EmpName,Department FROM Employees
+WHERE Department = 'IT';
+
+--3.Find employees whose salary is greater than 80,000.
+
+SELECT  EmpName ,Salary 
+FROM Employees
+WHERE Salary > 80000
+ORDER BY salary DESC;
+
+
+--4.List employees hired after 2020.
+
+SELECT EmpName ,HireDate  FROM Employees
+WHERE YEAR(HireDate) > 2020;
+
+
+--5..Show distinct departments.
+
+SELECT DISTINCT Department 
+FROM Employees;
 
